7790d7ae2a0aecaf6972991fdcb2e969adae33a2
Shameer Kamaludeen <shameer.plsn@gmail.com>
2022-07-26 17:42:07 +0530

    complete solution

--- scripts/thanks.js
+++ scripts/thanks.js
@@ -4,4 +4,4 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 let rating = params.rateRn;
 const note = document.getElementById('rc-rnb-note');
-note.textContent = `You selected ${rating} out of 5`;
+note.firstElementChild.textContent = `You selected ${rating} out of 5`;
